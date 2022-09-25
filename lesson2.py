3cf371f38189b72f61274e9054becf32902f2d15
lukabendeliani25 <lukabendeliani9@gmail.com>
2022-09-25 21:58:11 +0400

    day2coding

---
+++ lesson2.py
@@ -0,0 +1,47 @@
+name = "luka"
+print(len(name))
+
+my_sentence = """გამარჯობათ,მე ვარ ილიას სახელმწიფო უნივერსიტეტის სტუდენტი,
+ასევე გახლავართ სპორცმენი და ამასთანავე პროგრამისტი"""
+print(my_sentence)
+
+if 12>4:
+    print("hi")
+
+full_name = "luka bendeliani"
+print(len(full_name))
+
+print(full_name[14])
+print(full_name[6])
+
+print("b" in full_name)
+print("j" not in full_name)
+
+full_name = "luka bendeliani"
+print(full_name[3:8])
+print(full_name[3:12:3])
+
+print(full_name[:6])
+print(full_name[7:])
+
+print(full_name[-10])
+print(full_name[-1:-7:-1])
+
+print(full_name.upper())
+name2 = "LUKA"
+print(name2.lower())
+name3 = "   luka   "
+print(name3.strip())
+
+print(name3.replace(" ", "k"))
+print(full_name.replace("a", "$"))
+
+
+
+
+
+
+
+
+
+
